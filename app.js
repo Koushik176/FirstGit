f4874f98378644a89ba5ee418961fab9b1aebe54
Koushik176 <koushikashok176@gmail.com>
2023-03-09 14:04:26 +0530

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('welcome to git')
+// EXAMINE THE DOCUMENT OBJECT //
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+
+const headerTitle = document.getElementById('header-title');
+const header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px black';
+const addItem = document.getElementsByClassName('title');
+addItem[0].style.fontWeight = 'bold';
+addItem[0].style.color = 'green';
